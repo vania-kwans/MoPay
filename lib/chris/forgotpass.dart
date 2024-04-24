9c95e2aee5c61316daa7632f75921d48dd1d246b
Christine Gleania <221110164@students.mikroskil.ac.id>
2024-04-24 17:47:40 +0700

    proses pengerjaan

---
+++ lib/chris/forgotpass.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class forgotpass extends StatefulWidget {
+  const forgotpass({super.key});
+
+  @override
+  State<forgotpass> createState() => _forgotpassState();
+}
+
+class _forgotpassState extends State<forgotpass> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Halaman ini dalam proses pengerjaan"),
+    );
+  }
+}
