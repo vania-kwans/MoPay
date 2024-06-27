85cf80b6326307e1b229e9e8ab63c8a68299bc07
Sam! <156488637+sammmms@users.noreply.github.com>
2024-06-28 04:45:04 +0700

    feat: added save login preferences

--- lib/bloc/store.dart
+++ lib/bloc/store.dart
@@ -1,18 +1,43 @@
+import 'dart:convert';
+
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Store {
-  static void setToken(String token) async {
+  static Future<void> setToken(String token) async {
     final prefs = await SharedPreferences.getInstance();
     prefs.setString('token', token);
   }
 
-  static void getToken() async {
+  static Future<String?> getToken() async {
     final prefs = await SharedPreferences.getInstance();
-    prefs.getString('token');
+    return prefs.getString('token');
   }
 
-  static void removeToken() async {
+  static Future<void> removeToken() async {
     final prefs = await SharedPreferences.getInstance();
     prefs.remove('token');
+  }
+
+  static Future<void> saveLoginPreferences(
+      bool rememberMe, String phoneNumber, String password) async {
+    final prefs = await SharedPreferences.getInstance();
+    if (!rememberMe) {
+      prefs.remove('loginPreferences');
+      return;
+    }
+    Map<String, dynamic> loginPreferences = {
+      'rememberPassword': rememberMe,
+      'phoneNumber': phoneNumber,
+      'password': password,
+    };
+    prefs.setString('loginPreferences', jsonEncode(loginPreferences));
+  }
+
+  static Future<Map<String, dynamic>> getLoginPreferences() async {
+    final prefs = await SharedPreferences.getInstance();
+    String? loginPreferences = prefs.getString('loginPreferences');
+    if (loginPreferences == null) return {};
+    Map<String, dynamic> loginPreferencesMap = jsonDecode(loginPreferences);
+    return loginPreferencesMap;
   }
 }
