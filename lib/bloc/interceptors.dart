11d6fa6b0dc26a2a6a882d16f8caeadad8d57af3
Sam! <156488637+sammmms@users.noreply.github.com>
2024-06-28 04:44:00 +0700

    feat: added interceptors

--- lib/bloc/interceptors.dart
+++ lib/bloc/interceptors.dart
@@ -1,8 +1,12 @@
 import 'package:dio/dio.dart';
+import 'package:mopay_ewallet/bloc/store.dart';
 
 class TokenInterceptors extends InterceptorsWrapper {
   @override
-  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+  void onRequest(
+      RequestOptions options, RequestInterceptorHandler handler) async {
+    String? token = await Store.getToken();
+    options.headers.addAll({'Authorization': 'Bearer $token'});
     super.onRequest(options, handler);
   }
 }
