b55770c7ea4c757ba131cf5363e9ffdb42ee78e9
Vania <221110551@students.mikroskil.ac.id>
2024-06-27 09:18:22 +0700

    Mengupdate dan memperbarui data yang ada di dalam folder data

--- lib/data/data_saldo.dart
+++ lib/data/data_saldo.dart
@@ -1,17 +1,94 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class SaldoProvider extends ChangeNotifier {
-//   int saldo;
+class Balances {
+  int userID;
+  int saldo;
 
-//   SaldoProvider({this.saldo = 200000});
+  Balances({
+    required this.userID,
+    this.saldo = 0,
+  });
+}
 
-//   void addSaldo(int amount) {
-//     saldo += amount;
-//     notifyListeners();
-//   }
+class BalancesProvider extends ChangeNotifier {
+  Balances? currentBalance;
 
-//   void subtractSaldo(int amount) {
-//     saldo -= amount;
-//     notifyListeners();
-//   }
-// }
+  List<Balances> data = [
+    Balances(
+      userID: 1,
+      saldo: 5000000,
+    ),
+    Balances(
+      userID: 2,
+      saldo: 5000000,
+    ),
+    Balances(
+      userID: 3,
+      saldo: 5000000,
+    ),
+    Balances(
+      userID: 4,
+      saldo: 5000000,
+    ),
+    Balances(
+      userID: 5,
+      saldo: 5000000,
+    ),
+    Balances(
+      userID: 6,
+      saldo: 5000000,
+    ),
+    Balances(
+      userID: 7,
+      saldo: 5000000,
+    ),
+    Balances(
+      userID: 8,
+      saldo: 5000000,
+    ),
+    Balances(
+      userID: 9,
+      saldo: 5000000,
+    ),
+    Balances(
+      userID: 10,
+      saldo: 5000000,
+    ),
+    Balances(
+      userID: 11,
+      saldo: 5000000,
+    ),
+    Balances(
+      userID: 12,
+      saldo: 5000000,
+    ),
+    Balances(
+      userID: 13,
+      saldo: 5000000,
+    ),
+    Balances(
+      userID: 14,
+      saldo: 5000000,
+    ),
+    Balances(
+      userID: 15,
+      saldo: 5000000,
+    ),
+  ];
+
+  void setCurrentUserBalance(int userID) {
+    Balances? balance = data.firstWhere((balance) => balance.userID == userID);
+    currentBalance = balance;
+    notifyListeners();
+  }
+
+  void addBalance(int amount) {
+    currentBalance!.saldo += amount;
+    notifyListeners();
+  }
+
+  void subtractBalance(int amount) {
+    currentBalance!.saldo -= amount;
+    notifyListeners();
+  }
+}
