1cf477a4dff3fa10c4f2e5c9a8c49afa0950e8ef
Vania <221110551@students.mikroskil.ac.id>
2024-06-27 09:19:55 +0700

    Melakukan update pada tampilan home

--- lib/pages/home/home_upper.dart
+++ lib/pages/home/home_upper.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:mopay_ewallet/data/data_saldo.dart';
 import 'package:mopay_ewallet/format/currency.dart';
 import 'package:provider/provider.dart';
-import 'package:mopay_ewallet/data/data_user_mopay.dart';
-import 'package:mopay_ewallet/pages/history.dart';
+import 'package:mopay_ewallet/pages/history/history.dart';
 import 'package:mopay_ewallet/pages/top_up/top_up_methods.dart';
 import 'package:mopay_ewallet/pages/transfer/transfer_page.dart';
 
@@ -22,152 +22,148 @@ class _HomeUpperState extends State<HomeUpper> {
 
   @override
   Widget build(BuildContext context) {
-    MopayUserData currentUser =
-        Provider.of<MopayUserDataProvider>(context).currentUser;
-
-    return Column(
+    Balances? currentUserBalance =
+        Provider.of<BalancesProvider>(context, listen: false).currentBalance;
+    return Stack(
       children: [
-        Stack(
-          children: [
-            Container(
-              margin: const EdgeInsets.only(
-                left: 10,
-                top: 10,
-                bottom: 20,
-              ),
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.width / 2 + 20,
-              decoration: BoxDecoration(
-                gradient: const LinearGradient(
-                  colors: ([
-                    Color(0xff496989),
-                    Color.fromARGB(255, 26, 50, 82)
-                  ]),
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                ),
-                borderRadius: BorderRadius.circular(20),
-              ),
+        const SizedBox(height: 20),
+        Container(
+          margin: const EdgeInsets.only(
+            left: 10,
+            top: 10,
+            bottom: 20,
+          ),
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.width / 2 + 20,
+          decoration: BoxDecoration(
+            gradient: const LinearGradient(
+              colors: ([Color(0xff496989), Color.fromARGB(255, 26, 50, 82)]),
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
             ),
-            Container(
-              padding: const EdgeInsets.all(20),
-              margin: const EdgeInsets.only(right: 10),
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                gradient: const LinearGradient(
-                  colors: ([Color(0xff0F0E0E), Color(0xff850000)]),
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                ),
-                borderRadius: BorderRadius.circular(20),
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(20),
+          margin: const EdgeInsets.only(right: 10),
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            gradient: const LinearGradient(
+              colors: ([Color(0xff0F0E0E), Color(0xff850000)]),
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "MoPay Cash",
+                style: TextStyle(color: Colors.white, fontSize: 18),
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              Row(
                 children: [
                   const Text(
-                    "MoPay Cash",
-                    style: TextStyle(color: Colors.white, fontSize: 18),
+                    "Total Balance",
+                    style: TextStyle(color: Colors.white),
                   ),
-                  Row(
-                    children: [
-                      const Text(
-                        "Total Balance",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      IconButton(
-                        onPressed: () {
-                          setState(() {
-                            isObscured = !isObscured;
-                          });
-                        },
-                        icon: Icon(
-                          isObscured
-                              ? Icons.visibility_off_rounded
-                              : Icons.remove_red_eye_rounded,
-                          color: Colors.white,
-                          size: 16,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        isObscured
-                            ? 'Rp${obscuredText(currentUser.saldo)}'
-                            : 'Rp${formatToIndonesianCurrency(currentUser.saldo)}',
-                        style: const TextStyle(
-                          fontSize: 20,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(40),
-                        ),
-                        padding: const EdgeInsets.all(5),
-                        child: const Row(
-                          children: [
-                            CircleAvatar(
-                              radius: 10,
-                              backgroundColor: Color(0xff0E0548),
-                              child: Icon(
-                                Icons.currency_ruble,
-                                color: Colors.white,
-                                size: 20,
-                              ),
-                            ),
-                            SizedBox(width: 10),
-                            Text(
-                              'Mopay Points',
-                              style: TextStyle(fontSize: 12),
-                            ),
-                            Icon(
-                              Icons.arrow_forward_ios_sharp,
-                              size: 10,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 5),
-                  //FITUR UTAMA -------------------------------------------------------------------------------------------------------------------
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      createFitureButton(
-                        context: context,
-                        namaFitur: 'Top Up',
-                        icon: const Icon(
-                          Icons.add_circle_outline,
-                        ),
-                      ),
-                      createFitureButton(
-                        context: context,
-                        namaFitur: 'Transfer',
-                        icon: const Icon(Icons.arrow_circle_up_outlined),
-                      ),
-                      createFitureButton(
-                        context: context,
-                        namaFitur: 'Tarik Tunai',
-                        icon: const Icon(Icons.arrow_circle_down_rounded),
-                      ),
-                      createFitureButton(
-                        context: context,
-                        namaFitur: 'History',
-                        icon: const Icon(Icons.history),
-                      ),
-                    ],
+                  IconButton(
+                    onPressed: () {
+                      setState(() {
+                        isObscured = !isObscured;
+                      });
+                    },
+                    icon: Icon(
+                      isObscured
+                          ? Icons.visibility_off_rounded
+                          : Icons.remove_red_eye_rounded,
+                      color: Colors.white,
+                      size: 16,
+                    ),
                   ),
                 ],
               ),
-            )
-          ],
-        ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    isObscured
+                        ? 'Rp${obscuredText(currentUserBalance!.saldo)}'
+                        : 'Rp${formatToIndonesianCurrency(currentUserBalance!.saldo)}',
+                    // isObscured
+                    //     ? 'Rp${obscuredText(currentUserBalance.saldo)}'
+                    //     : 'Rp${formatToIndonesianCurrency(currentUserBalance.saldo)}',
+                    style: const TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    padding: const EdgeInsets.all(5),
+                    child: const Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 10,
+                          backgroundColor: Color(0xff0E0548),
+                          child: Icon(
+                            Icons.currency_ruble,
+                            color: Colors.white,
+                            size: 20,
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          'Mopay Points',
+                          style: TextStyle(fontSize: 12),
+                        ),
+                        Icon(
+                          Icons.arrow_forward_ios_sharp,
+                          size: 10,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 5),
+              //FITUR UTAMA -------------------------------------------------------------------------------------------------------------------
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  createFitureButton(
+                    context: context,
+                    namaFitur: 'Top Up',
+                    icon: const Icon(
+                      Icons.add_circle_outline,
+                    ),
+                  ),
+                  createFitureButton(
+                    context: context,
+                    namaFitur: 'Transfer',
+                    icon: const Icon(Icons.arrow_circle_up_outlined),
+                  ),
+                  createFitureButton(
+                    context: context,
+                    namaFitur: 'Tarik Tunai',
+                    icon: const Icon(Icons.arrow_circle_down_rounded),
+                  ),
+                  createFitureButton(
+                    context: context,
+                    namaFitur: 'History',
+                    icon: const Icon(Icons.history),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        )
       ],
     );
   }
