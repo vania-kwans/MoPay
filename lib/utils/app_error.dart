69446812a0612928d089c3003fc7884265579e35
sammmms <samuelonasis23@gmail.com>
2024-06-29 11:29:07 +0700

    fix: unhandled no internet connection error

--- lib/utils/app_error.dart
+++ lib/utils/app_error.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 
 class AppError {
@@ -8,6 +10,9 @@ class AppError {
 
   factory AppError.fromObjectErr(Object? err) {
     if (err is DioException) {
+      if (err.error is SocketException) {
+        return AppError(message: 'No internet connection');
+      }
       return AppError(
           message: err.response?.data,
           code: err.response?.statusCode.toString());
