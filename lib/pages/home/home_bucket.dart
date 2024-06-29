f26d045bfa15e186a2bbaba647ae32c93f0f4514
Sam! <156488637+sammmms@users.noreply.github.com>
2024-06-30 06:15:08 +0700

    fix: remove loading dialog so home page ads could show

--- lib/pages/home/home_bucket.dart
+++ lib/pages/home/home_bucket.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
-import 'package:lottie/lottie.dart';
 import 'package:mopay_ewallet/bloc/auth/auth_bloc.dart';
 import 'package:mopay_ewallet/bloc/store.dart';
 import 'package:mopay_ewallet/pages/history/history.dart';
@@ -35,16 +34,6 @@ class _HomeBucketState extends State<HomeBucket> {
   void initState() {
     bloc = context.read<AuthBloc>();
     SchedulerBinding.instance.addPostFrameCallback((_) async {
-      // Loading Dialog
-      showDialog(
-          context: context,
-          builder: (context) => PopScope(
-                canPop: false,
-                child: Dialog(
-                  child: Lottie.asset('assets/lottie/mopayLottie.json'),
-                ),
-              ));
-
       DateTime lastPinEnter = await Store.getLastPinEnter();
       // Jika user sudah 5 menit tidak memasukkan pin, maka user harus memasukkan pin
       if (lastPinEnter
@@ -59,21 +48,13 @@ class _HomeBucketState extends State<HomeBucket> {
           ),
         );
 
-        // Jika user sudah memasukkan pin, maka langsung ke home
-        if (!mounted) return;
-        Navigator.pop(context);
-
         if (isPinValid == null || !isPinValid) {
+          if (!mounted) return;
           Navigator.pushNamedAndRemoveUntil(
               context, '/login', (route) => false);
         }
         return;
       }
-
-      // Jika user sudah memasukkan pin, maka langsung ke home
-      await Future.delayed(const Duration(seconds: 1));
-      if (!mounted) return;
-      Navigator.pop(context);
     });
     super.initState();
   }
@@ -81,188 +62,79 @@ class _HomeBucketState extends State<HomeBucket> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageStorage(bucket: bucket, child: screens[currentTab]),
-      floatingActionButton: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(30.0),
-          border: Border.all(color: Colors.white, width: 3.0),
-        ),
-        child: FloatingActionButton(
-          onPressed: () {},
-          shape: const CircleBorder(),
-          backgroundColor: const Color(0xff850000),
-          foregroundColor: Colors.white,
-          child: Icon(
-            Icons.qr_code_2,
-            size: MediaQuery.of(context).size.width / 4 * 0.5,
+        body: PageStorage(bucket: bucket, child: screens[currentTab]),
+        floatingActionButton: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30.0),
+            border: Border.all(color: Colors.white, width: 3.0),
+          ),
+          child: FloatingActionButton(
+            onPressed: () {},
+            shape: const CircleBorder(),
+            backgroundColor: const Color(0xff850000),
+            foregroundColor: Colors.white,
+            child: Icon(
+              Icons.qr_code_2,
+              size: MediaQuery.of(context).size.width / 4 * 0.5,
+            ),
           ),
         ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: Container(
-        color: Colors.transparent,
-        height: MediaQuery.of(context).size.height * 0.07,
-        child: BottomAppBar(
-          elevation: 0,
-          padding: const EdgeInsets.all(0),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        bottomNavigationBar: Container(
           color: Colors.transparent,
-          shape: const CircularNotchedRectangle(),
-          notchMargin: 15.0,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              // Bagian kiri
-              SizedBox(
-                width: MediaQuery.of(context).size.width / 2 - 20,
-                child: Row(
-                  children: [
-                    MaterialButton(
-                      padding: const EdgeInsets.all(0),
-                      onPressed: () {
-                        setState(() {
-                          // currentScreen = const PilihanMetodeTopUpPage();
-                          currentTab = 0;
-                        });
-                      },
-                      child: SizedBox(
-                        width: MediaQuery.of(context).size.width / 4 - 20,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(
-                              Icons.home,
-                              color: currentTab == 0
-                                  ? const Color(0xff850000)
-                                  : Colors.grey,
-                              size:
-                                  MediaQuery.of(context).size.width / 4 * 0.25,
-                            ),
-                            Text(
-                              'Home',
-                              style: TextStyle(
-                                color: currentTab == 0
-                                    ? const Color(0xff850000)
-                                    : Colors.grey,
-                                fontSize: MediaQuery.of(context).size.width /
-                                    4 *
-                                    0.125,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    MaterialButton(
-                      padding: const EdgeInsets.all(0),
-                      onPressed: () {
-                        setState(() {
-                          // currentScreen = const PilihanMetodeTopUpPage();
-                          currentTab = 1;
-                        });
-                      },
-                      child: SizedBox(
-                        width: MediaQuery.of(context).size.width / 4 - 20,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(
-                              Icons.history,
-                              color: currentTab == 1
-                                  ? const Color(0xff850000)
-                                  : Colors.grey,
-                              size:
-                                  MediaQuery.of(context).size.width / 4 * 0.25,
-                            ),
-                            Text(
-                              'History',
-                              style: TextStyle(
-                                color: currentTab == 1
-                                    ? const Color(0xff850000)
-                                    : Colors.grey,
-                                fontSize: MediaQuery.of(context).size.width /
-                                    4 *
-                                    0.125,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+          height: MediaQuery.of(context).size.height * 0.07,
+          child: BottomAppBar(
+              elevation: 0,
+              padding: const EdgeInsets.all(0),
+              color: Colors.transparent,
+              shape: const CircularNotchedRectangle(),
+              notchMargin: 15.0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  // Bagian kiri
+                  _bottomNavbarButton(Icons.home, "Home", 0),
+
+                  _bottomNavbarButton(Icons.history, "History", 1),
+
+                  const Spacer(),
+                  // Bagian kanan
+                  _bottomNavbarButton(Icons.notifications, "Notification", 2),
+                  _bottomNavbarButton(Icons.account_circle, "Profile", 3)
+                ],
+              )),
+        ));
+  }
+
+  Widget _bottomNavbarButton(IconData icon, String text, int index) {
+    return Flexible(
+      flex: 2,
+      child: MaterialButton(
+        padding: const EdgeInsets.all(0),
+        onPressed: () {
+          setState(() {
+            // currentScreen = const PilihanMetodeTopUpPage();
+            currentTab = index;
+          });
+        },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              icon,
+              color:
+                  currentTab == index ? const Color(0xff850000) : Colors.grey,
+              size: MediaQuery.of(context).size.width / 4 * 0.25,
+            ),
+            Text(
+              text,
+              style: TextStyle(
+                color:
+                    currentTab == index ? const Color(0xff850000) : Colors.grey,
+                fontSize: MediaQuery.of(context).size.width / 4 * 0.125,
               ),
-              const SizedBox(width: 40),
-              // Bagian kanan
-              MaterialButton(
-                padding: const EdgeInsets.all(0),
-                onPressed: () {
-                  setState(() {
-                    // currentScreen = const PilihanMetodeTopUpPage();
-                    currentTab = 2;
-                  });
-                },
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width / 4 - 20,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.notifications,
-                        color: currentTab == 2
-                            ? const Color(0xff850000)
-                            : Colors.grey,
-                        size: MediaQuery.of(context).size.width / 4 * 0.25,
-                      ),
-                      Text(
-                        'Notification',
-                        style: TextStyle(
-                          color: currentTab == 2
-                              ? const Color(0xff850000)
-                              : Colors.grey,
-                          fontSize:
-                              MediaQuery.of(context).size.width / 4 * 0.125,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              MaterialButton(
-                padding: const EdgeInsets.all(0),
-                onPressed: () {
-                  setState(() {
-                    // currentScreen = const PilihanMetodeTopUpPage();
-                    currentTab = 3;
-                  });
-                },
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width / 4 - 20,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.account_circle,
-                        color: currentTab == 3
-                            ? const Color(0xff850000)
-                            : Colors.grey,
-                        size: MediaQuery.of(context).size.width / 4 * 0.25,
-                      ),
-                      Text(
-                        'Profile',
-                        style: TextStyle(
-                          color: currentTab == 3
-                              ? const Color(0xff850000)
-                              : Colors.grey,
-                          fontSize:
-                              MediaQuery.of(context).size.width / 4 * 0.125,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
