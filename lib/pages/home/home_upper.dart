15c99c4c38606586b44abb37f320ae01ee95879a
elly <221110139@students.mikroskil.ac.id>
2024-07-06 12:09:41 +0700

    rapikan nama item

--- lib/pages/home/home_upper.dart
+++ lib/pages/home/home_upper.dart
@@ -162,7 +162,7 @@ class _HomeUpperState extends State<HomeUpper> {
                   ),
                   createFitureButton(
                     context: context,
-                    namaFitur: 'Cash',
+                    namaFitur: 'Tarik Tunai',
                     icon: const Icon(Icons.arrow_circle_down_rounded),
                   ),
                   createFitureButton(
