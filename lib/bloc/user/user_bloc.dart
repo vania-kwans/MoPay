c536c16567ea1b1466e4250b279512db118fbc39
Vania <221110551@students.mikroskil.ac.id>
2024-07-07 10:00:53 +0700

    Update: function

--- lib/bloc/user/user_bloc.dart
+++ lib/bloc/user/user_bloc.dart
@@ -6,7 +6,9 @@ import 'package:mopay_ewallet/bloc/interceptors.dart';
 import 'package:mopay_ewallet/bloc/user/user_state.dart';
 import 'package:mopay_ewallet/models/user.dart';
 import 'package:mopay_ewallet/utils/app_error.dart';
+import 'package:mopay_ewallet/utils/picture_type_util.dart';
 import 'package:mopay_ewallet/utils/print_error.dart';
+import 'package:provider/provider.dart';
 import 'package:rxdart/rxdart.dart';
 
 class UserBloc {
@@ -19,9 +21,7 @@ class UserBloc {
 
   final AuthBloc authBloc;
 
-  UserBloc(
-    this.authBloc,
-  ) {
+  UserBloc(this.authBloc) {
     dio.interceptors.add(TokenInterceptors());
   }
 
@@ -55,7 +55,9 @@ class UserBloc {
     try {
       var response = await dio.get("/profile");
       if (response.statusCode == 201) {
-        _updateStream(UserState.success(User.fromJson(response.data)));
+        User user = User.fromJson(response.data);
+        _updateStream(UserState.success(user));
+        // currentUserProvider.setCurrentUser(user);
       } else {
         _updateError(response);
       }
@@ -93,6 +95,55 @@ class UserBloc {
       printError(err);
       AppError error = AppError.fromObjectErr(err);
       publicProfileController.add(UserState.error(error));
+    }
+  }
+
+  Future<void> updateProfilePicture(
+      String newPictureLink, PictureType newPictureType) async {
+    _updateStream(UserState.loading());
+    try {
+      var response = await dio.patch("/profile/picture", data: {
+        'pictureLink': newPictureLink,
+        'pictureType': PictureTypeUtil.textOf(newPictureType),
+      });
+      if (response.statusCode == 200) {
+        User? user = controller.valueOrNull?.user?.copyWith(
+          pictureLink: newPictureLink,
+          pictureType: newPictureType,
+        );
+        if (user == null) {
+          throw Exception("User is null");
+        }
+        _updateStream(UserState.success(user));
+      } else {
+        _updateError(response);
+      }
+    } catch (err) {
+      printError(err);
+      _updateError(err);
+    }
+  }
+
+  Future<void> updateUserName(String newName) async {
+    _updateStream(UserState.loading());
+    try {
+      var response = await dio.patch("/profile", data: {
+        'name': newName,
+      });
+      if (response.statusCode == 200) {
+        User? user = controller.valueOrNull?.user?.copyWith(
+          name: newName,
+        );
+        if (user == null) {
+          throw Exception("User is null");
+        }
+        _updateStream(UserState.success(user));
+      } else {
+        _updateError(response);
+      }
+    } catch (err) {
+      printError(err);
+      _updateError(err);
     }
   }
 }
